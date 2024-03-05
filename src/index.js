3394d4d1a6f7ff9489296c1ab4ae72e64104c8fe
AmandaNcaps <158183848+AmandaNcaps@users.noreply.github.com>
2024-03-05 20:29:03 +0200

    testing with console.log
    adding function to refresh weather

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
+function refreshWeather(response) {
+  console.log(response.data);
+}
+
+function searchCity(city) {
+  let apiKey = "423ato3d442d8f9abd0e1f2baa6efd19";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
